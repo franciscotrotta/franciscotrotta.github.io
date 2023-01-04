4c54a01c6c38ed54908b452f3c0335773b0bf8f6
Francisco Trotta <franciscotrotta@gmail.com>
2023-01-04 15:27:56 -0300

    Añadido script básico JS (alerta al submitear formulario), cambios menores redacción

---
+++ cv/js/javascript cv.js
@@ -0,0 +1,8 @@
+
+function confirmarEnvio(){
+    alert("¡Gracias! Tu consulta será respondida lo antes posible");
+}
+
+
+const form = document.getElementById("formulario-contacto");
+form.addEventListener('submit', confirmarEnvio);
