e59460ccb0e2406196900853e57b7b9f78820ecd
Francisco Trotta <franciscotrotta@gmail.com>
2023-01-05 16:17:43 -0300

    Añadidos temas claro y oscuro y variables de CSS vinculadas a JS

--- cv/js/javascript cv.js
+++ cv/js/javascript cv.js
@@ -6,3 +6,16 @@ function confirmarEnvio(){
 
 const form = document.getElementById("formulario-contacto");
 form.addEventListener('submit', confirmarEnvio);
+
+document.getElementById('temaclaro').addEventListener('click', function(){
+   document.body.style.backgroundColor = "var(--light-bg-color)";
+   document.body.style.color = "var(--light-color)";
+   document.getElementById("columnalateral").style.backgroundColor = "var(--col-light-color)";
+   
+})
+document.getElementById('temaoscuro').addEventListener('click', function(){
+ 
+    document.body.style.backgroundColor = "var(--dark-bg-color)";
+    document.body.style.color = "var(--dark-color)";
+    document.getElementById("columnalateral").style.backgroundColor = "var(--col-dark-color)";
+})
