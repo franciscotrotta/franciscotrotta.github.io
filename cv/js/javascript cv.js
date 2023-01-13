5d44079149cde3c8b5f970c8466cd21363dfa783
Francisco Trotta <franciscotrotta@gmail.com>
2023-01-13 17:40:58 -0300

    Añadida funcion con evento keydown y cambio en tamaño botones de tema

--- cv/js/javascript cv.js
+++ cv/js/javascript cv.js
@@ -1,4 +1,4 @@
-
+//función vinculada al formulario
 function confirmarEnvio(){
     alert("¡Gracias! Tu consulta será respondida lo antes posible");
 }
@@ -6,6 +6,7 @@ function confirmarEnvio(){
 const form = document.getElementById("formulario-contacto");
 form.addEventListener('submit', confirmarEnvio);
 
+//cambios de color en función del tema elegido
 let list = document.getElementsByTagName("ol");
 document.getElementById('temaclaro').addEventListener('click', function(){
    document.body.style.backgroundColor = "var(--light-bg-color)";
@@ -20,4 +21,12 @@ document.getElementById('temaoscuro').addEventListener('click', function(){
     document.getElementById("columnalateral").style.backgroundColor = "var(--col-dark-color)";
     list[0].style.backgroundColor = "rgb(49, 49, 49)";
     list[0].style.color = "var(--dark-color)";
+})
+
+//funciones de teclas
+
+window.addEventListener('keydown', function(event){
+    if(event.key == "h"||event.key == "H"){
+        alert("Tecla Inicio para volver al principio de la página\n" + "Tecla Fin para ir al final del documento.")
+    }
 })
